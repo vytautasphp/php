2f0303473d8fbf4c91e762e74b1f630faccd383a
vytautasphp <csvytas@gmail.com>
2017-11-14 21:38:55 +0200

    1.0

--- 05php/index.php
+++ 05php/index.php
@@ -1,14 +1,31 @@
 <?php
-$a = 3;
-$b=4; 
-$c=5;
+$a=[3, 2, 5, 5];
+$b=[4, 10, 6, 5]; 
+$c=[5, 8, 5, 5];
+$d=0;
 
-if($a+$b>$c&&$a+$c>$b&&$b+$c>$a){
-    
-   echo 'trikampis susidaro'; 
-}
+for ($i = 0; $i < count($a); $i++){
+    $e=$d;
+    echo ' |'.$e=$e+1; 
+if($a[$d]+$b[$d]>$c[$d]&&$a[$d]+$c[$d]>$b[$d]&&$b[$d]+$c[$d]>$a[$d]){   
+   echo ' trikampis susidaro';
+        if ($c[$d]==$a[$d] && $c[$d]==$b[$d] && $b[$d]==$a[$d]){
+            echo ' lygiakrastis ';
+            }
+            else if (c==$a[$d] && $c[$d]!=$b[$d] && $c[$d]!=$a[$d]|| $b[$d]==$a[$d] && $b[$d]!=$c[$d] && $a[$d]!=$c[$d]|| $c[$d]==$b[$d] && $c[$d]!=$a[$d] && $b[$d]!=$a[$d]){
+                echo ' lygiasonis ';
+            }
+            else if (pow($a[$d], 2)+pow($b[$d], 2)==pow($c[$d], 2)){
+                echo ' statusis ';
+            }
+            else {
+                echo ' ivairiakrastis ';
+            }
+        }
 else {
-    echo 'trikampis nesusidaro'; 
+    echo ' trikampis nesusidaro'; 
 }
 
+$d++;
+}
 ?>
