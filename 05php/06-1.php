f436d03540a5e3493e0b4adb23fffbb82a809adf
vytautasphp <csvytas@gmail.com>
2017-11-15 20:50:48 +0200

    1.0

---
+++ 05php/06-1.php
@@ -0,0 +1,19 @@
+<?php
+$a = [5, 6, 10, 15];
+$b = [8, 5, 3, 25];
+
+
+ function vid($a)   {
+        for ($i = 0; $i < count($a); $i++){
+            $vidurkis = ($vidurkis + $a[$i]);
+        }
+    $vidurkis = $vidurkis/count($a);
+    return $vidurkis;
+}
+$skirtumas = vid($a) - vid($b);
+echo 'Rezultatas  '.$skirtumas;
+
+?>
+
+
+
